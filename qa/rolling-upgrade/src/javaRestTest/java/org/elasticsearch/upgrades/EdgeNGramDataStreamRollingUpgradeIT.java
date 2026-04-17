7ec16e86d2c181a00084578d7acdf04c02a43f00
Salvatore Campagna <salvatore.campagna@elastic.co>
2026-04-17 09:20:26 +0200

    fix: simplify rolling upgrade test to show breakage
    The test no longer catches exceptions. It creates a template with
    edge_ngram(1,20) on the old cluster, rollovers successfully, then
    attempts rollover on the upgraded cluster. This will fail in CI
    due to the max_ngram_diff validation, demonstrating the BWC
    breaking change.

--- qa/rolling-upgrade/src/javaRestTest/java/org/elasticsearch/upgrades/EdgeNGramDataStreamRollingUpgradeIT.java
+++ qa/rolling-upgrade/src/javaRestTest/java/org/elasticsearch/upgrades/EdgeNGramDataStreamRollingUpgradeIT.java
@@ -12,9 +12,6 @@ package org.elasticsearch.upgrades;
 import com.carrotsearch.randomizedtesting.annotations.Name;
 
 import org.elasticsearch.client.Request;
-import org.elasticsearch.client.ResponseException;
-
-import static org.hamcrest.Matchers.containsString;
 
 public class EdgeNGramDataStreamRollingUpgradeIT extends AbstractRollingUpgradeTestCase {
 
@@ -53,61 +50,37 @@ public class EdgeNGramDataStreamRollingUpgradeIT extends AbstractRollingUpgradeT
             }
         }""";
 
-    private static final String VALIDATION_ERROR = "The difference between max_gram and min_gram in EdgeNGram Tokenizer "
-        + "must be less than or equal to";
-
     public EdgeNGramDataStreamRollingUpgradeIT(@Name("upgradedNodes") int upgradedNodes) {
         super(upgradedNodes);
     }
 
     public void testDataStreamWithEdgeNgramAnalyzer() throws Exception {
-        final boolean oldClusterHasValidation = oldClusterHasFeature("analysis.edge_ngram.max_ngram_diff_validation");
-
         if (isOldCluster()) {
             final Request putTemplate = new Request("PUT", "/_index_template/" + TEMPLATE_NAME);
             putTemplate.setJsonEntity(INDEX_TEMPLATE);
+            client().performRequest(putTemplate);
 
-            if (oldClusterHasValidation) {
-                final ResponseException ex = expectThrows(ResponseException.class, () -> client().performRequest(putTemplate));
-                assertThat(ex.getMessage(), containsString(VALIDATION_ERROR));
-            } else {
-                client().performRequest(putTemplate);
+            final Request createDataStream = new Request("PUT", "/_data_stream/" + DATA_STREAM_NAME);
+            client().performRequest(createDataStream);
 
-                final Request createDataStream = new Request("PUT", "/_data_stream/" + DATA_STREAM_NAME);
-                client().performRequest(createDataStream);
+            final Request indexDoc = new Request("POST", "/" + DATA_STREAM_NAME + "/_doc");
+            indexDoc.addParameter("refresh", "true");
+            indexDoc.setJsonEntity("""
+                {"@timestamp": "2026-01-01T00:00:00Z", "body": "test document"}
+                """);
+            client().performRequest(indexDoc);
 
-                final Request indexDoc = new Request("POST", "/" + DATA_STREAM_NAME + "/_doc");
-                indexDoc.addParameter("refresh", "true");
-                indexDoc.setJsonEntity("""
-                    {"@timestamp": "2026-01-01T00:00:00Z", "body": "test document"}
-                    """);
-                client().performRequest(indexDoc);
-
-                final Request rollover = new Request("POST", "/" + DATA_STREAM_NAME + "/_rollover");
-                client().performRequest(rollover);
-            }
+            final Request rollover = new Request("POST", "/" + DATA_STREAM_NAME + "/_rollover");
+            client().performRequest(rollover);
         } else if (isUpgradedCluster()) {
-            if (oldClusterHasValidation) {
-                // Template creation failed on the old cluster, nothing to rollover.
-                // Validate that template creation still fails on the upgraded cluster.
-                final Request putTemplate = new Request("PUT", "/_index_template/" + TEMPLATE_NAME);
-                putTemplate.setJsonEntity(INDEX_TEMPLATE);
-                final ResponseException ex = expectThrows(ResponseException.class, () -> client().performRequest(putTemplate));
-                assertThat(ex.getMessage(), containsString(VALIDATION_ERROR));
-            } else {
-                // Template was created on the old cluster without validation.
-                // Rollover fails on the upgraded cluster because the new backing
-                // index triggers validation.
-                final Request rollover = new Request("POST", "/" + DATA_STREAM_NAME + "/_rollover");
-                final ResponseException ex = expectThrows(ResponseException.class, () -> client().performRequest(rollover));
-                assertThat(ex.getMessage(), containsString(VALIDATION_ERROR));
+            final Request rollover = new Request("POST", "/" + DATA_STREAM_NAME + "/_rollover");
+            client().performRequest(rollover);
 
-                final Request deleteDataStream = new Request("DELETE", "/_data_stream/" + DATA_STREAM_NAME);
-                client().performRequest(deleteDataStream);
+            final Request deleteDataStream = new Request("DELETE", "/_data_stream/" + DATA_STREAM_NAME);
+            client().performRequest(deleteDataStream);
 
-                final Request deleteTemplate = new Request("DELETE", "/_index_template/" + TEMPLATE_NAME);
-                client().performRequest(deleteTemplate);
-            }
+            final Request deleteTemplate = new Request("DELETE", "/_index_template/" + TEMPLATE_NAME);
+            client().performRequest(deleteTemplate);
         }
     }
 }
